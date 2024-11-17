395065149101ba3c58c5ed723d5e20c9611f7296
Youtong0826 <jimmy2007826@gmail.com>
2024-11-18 01:33:36 +0800

    98-3

---
+++ 98/98-3/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace _98_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(72, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "D:\\test\\media\\ringoutXP.wav";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "聲音檔";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(425, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "開啟檔案並產生波型";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(72, 114);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(416, 158);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(72, 83);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(416, 17);
+            this.hScrollBar1.TabIndex = 4;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 57);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "調整顯示的位置";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 183);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(53, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 168);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "現在的位置";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(506, 168);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "總長度";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(506, 184);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(53, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 297);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ 98/98-3/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace _98_3
+{
+    public partial class Form1 : Form
+    {
+        int sampleSize, sample;
+        double len;
+        Bitmap bmp;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void raise()
+        {
+            MessageBox.Show("該檔案不是指定的格式", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BinaryReader rd = new BinaryReader(File.OpenRead(textBox1.Text));
+            if (new string(rd.ReadChars(4)) != "RIFF") 
+            { 
+                raise();
+                return;
+            }
+
+            rd.BaseStream.Seek(0x8, SeekOrigin.Begin);
+            // Console.WriteLine(rd.ReadChars(7));
+            if (new string(rd.ReadChars(7)) != "WAVEfmt")
+            {
+                raise();
+                return;
+            }
+            rd.BaseStream.Seek(0x14, SeekOrigin.Begin);
+            // Console.WriteLine(rd.ReadInt16());
+            // Console.WriteLine(rd.ReadInt16());
+            if (rd.ReadInt16() != 1)
+            {
+                raise();
+                return;
+            }
+            // Console.WriteLine(rd.ReadInt16());
+            if (rd.ReadInt16() != 1)
+            {
+                raise();
+                return;
+            }
+            sample = rd.ReadInt32();
+            Console.WriteLine(sample);
+            rd.BaseStream.Seek(0x22, SeekOrigin.Begin);
+            // Console.WriteLine(rd.ReadInt16());
+            if (rd.ReadInt16() != 8)
+            {
+                raise();
+                return;
+            }
+
+            rd.BaseStream.Seek(0x28, SeekOrigin.Begin);
+            sampleSize = rd.ReadInt32();
+            len = (float)sampleSize / (float)sample;
+
+            bmp = new Bitmap(sampleSize, pictureBox1.Height);
+            var g = Graphics.FromImage(bmp);
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.Clear(Color.Black);
+            rd.BaseStream.Seek(0x2C, SeekOrigin.Begin);
+            for (int i = 0; i < sampleSize; i++)
+            {
+                int val = rd.ReadByte(); // 0 ~ 255
+                // 128 = middle
+                float value;
+                // if (val == 0x80) continue;
+                if (val > 0x80)
+                {
+                    value = (val - 0x80) / (float)(0xFF - 0x80);
+                }
+                else
+                {
+                    value = -(val - 0x80) / (float)(0xFF - 0x80);
+                }
+
+                value *= pictureBox1.Height;
+                g.DrawLine(Pens.Green, i, (pictureBox1.Height - value) * 0.5f, i, (pictureBox1.Height + value) * 0.5f);
+            }
+            hScrollBar1.Maximum = sampleSize - pictureBox1.Width;
+            hScrollBar1.Minimum = 0;
+            hScrollBar1.Value = 0;
+            textBox2.Text = $"{0f:f5}";
+            textBox3.Text = $"{len:f5}";
+            Bitmap bmp2 = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp2);
+            g.DrawImageUnscaled(bmp, -hScrollBar1.Value, 0);
+            pictureBox1.Image = bmp2;
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            if (bmp == null)
+                return;
+            Bitmap bmp2 = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(bmp2);
+            // Console.WriteLine(hScrollBar1.Value);
+            g.DrawImageUnscaled(bmp, -hScrollBar1.Value, 0);
+            pictureBox1.Image = bmp2;
+            textBox2.Text = $"{(float)hScrollBar1.Value / (float)sample:f5}";
+        }
+    }
+}
