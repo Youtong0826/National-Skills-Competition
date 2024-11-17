254bbdfd0ddfe7f89b3b9660a4f7882e3f8ea9d8
Youtong0826 <jimmy2007826@gmail.com>
2024-11-17 17:49:29 +0800

    94-4

---
+++ 94/94-4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _94_4
+{
+    internal class Program
+    {
+        static int calc(int x, double y, double z)
+        {
+            return (int)Math.Round(x * y / z);
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                List<int> o = new List<int>();
+                for (int i = 0; i < 4; i++)
+                {
+                    var d = Console.ReadLine().Split();
+                    var x = Array.ConvertAll(d[1].Split('/'), Convert.ToDouble);
+                    var y = x[0] / x[1];
+                    o.Add(calc(Convert.ToInt32(d[0]), y, Convert.ToDouble(d[2])));
+                }
+
+                foreach(var t in o)
+                {
+                    Console.WriteLine(t);
+                }
+            }
+        }
+    }
+}
