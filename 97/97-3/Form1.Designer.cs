0b162f166841c2b79e4c9460c6a931a1bf734dfd
Youtong0826 <jimmy2007826@gmail.com>
2024-11-17 17:52:36 +0800

    97-3

---
+++ 97/97-3/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace _97_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Real Number";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(156, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(208, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "-161.875";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(156, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(208, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "24 Bit Binary Number";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(156, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(289, 144);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 193);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 97/97-3/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _97_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num = Convert.ToDouble(textBox1.Text);
+            List<string> list = new List<string>();
+            string[] bit16 = new string[16] {
+                "0", "0", "0", "0", "0", "0", "0", "0", 
+                "0", "0", "0", "0", "0", "0", "0", "0", 
+            }, bit8 = new string[8] {
+                 "0", "0", "0", "0", "0", "0", "0", "0" 
+            };
+
+            if (num < 0) bit16[0] = "1";
+            num = Math.Abs(num);
+            int num1 = (int)Math.Floor(num);
+            double num2 = num - num1;
+            int stx = 15;
+            while (num1 > 0)
+            {
+                if (stx < 1)
+                {
+                    textBox2.Text = "overflow";
+                    return;
+                }
+                bit16[stx--] = $"{num1 % 2}";
+                // list.Add($"{num % 2}");
+                num1 /= 2;
+            }
+
+            stx = 0;
+            
+            while (num2 > 0)
+            {
+                Console.WriteLine(num2);
+                if (stx > 7)
+                {
+                    textBox2.Text = "overflow";
+                    return;
+                }
+                num2 *= 2;
+                bit8[stx++] = num2 > 0 ? "1" : "0";
+                num2 -= num2 >= 1 ? 1 : 0;
+            }
+
+            textBox2.Text = string.Join("", bit16) + "." + string.Join("", bit8) ;
+            // foreach (string str in list) { Console.WriteLine(str); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
