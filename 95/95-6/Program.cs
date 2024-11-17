6beded3ac1b11816f9bfdcf9dbc5e6b4286e17c5
Youtong0826 <jimmy2007826@gmail.com>
2024-11-17 17:51:10 +0800

    95-6

---
+++ 95/95-6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _95_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var s = Console.ReadLine();
+                Dictionary<char, int> mp = new Dictionary<char, int>();
+                List<char> list = new List<char>();
+                foreach (var c in s){
+                    if (mp.ContainsKey(c))
+                        mp[c]++;
+                    else
+                    {
+                        mp[c] = 1;
+                        list.Add(c);
+                    }
+                }
+
+                list.Sort((a, b) => mp[b] - mp[a]);
+                foreach (var c in list)
+                {
+                    Console.Write($"\"{c}\"={mp[c]}; ");
+                }
+            }
+        }
+    }
+}
