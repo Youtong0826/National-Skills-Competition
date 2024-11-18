f74bc8ebb89ffdd862039ed90dc6fb0936a5e9ac
Youtong0826 <jimmy2007826@gmail.com>
2024-11-18 13:14:45 +0800

    98-5

---
+++ 98/98-5/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace _98_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "請輸入點的數量";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(130, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(48, 25);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(419, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "計算最大距離";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(419, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 25);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "計算最短距離";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(419, 76);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 25);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "計算平均距離";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(419, 107);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 26);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "畫出點的分布";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(419, 139);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Color = System.Drawing.Color.Red;
+            series1.Legend = "Legend1";
+            series1.Name = "資料點";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.Color = System.Drawing.Color.DodgerBlue;
+            series2.Legend = "Legend1";
+            series2.Name = "基準點";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(394, 320);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            this.chart1.Visible = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(13, 46);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(400, 388);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "請輸入 N 點座標";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(317, 15);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(45, 25);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "請輸入測試點座標";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(368, 15);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(45, 25);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(540, 13);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 25);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(540, 45);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 25);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(540, 76);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 25);
+            this.textBox6.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 471);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ 98/98-5/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Math;
+using static System.Console;
+using static System.Convert;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace _98_5
+{
+    public partial class Form1 : Form
+    {
+        double minV, maxV, Avr;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private (List<double> X, List<double> Y) getPos()
+        {
+            List<double> X = new List<double>(), Y = new List<double>();
+            foreach (var item in groupBox1.Controls)
+            {
+                TextBox txt;
+                try { txt = (TextBox)item; }
+                catch { continue; }
+                try
+                {
+                    if (txt.Name.StartsWith("X")) X.Add(ToDouble(txt.Text));
+                    else Y.Add(ToDouble(txt.Text));
+                }
+                catch
+                {
+                    MessageBox.Show("資料點格式錯誤", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+                }
+            }
+
+            return (X, Y);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int N;
+            if (textBox1.Text == "")
+            {
+                groupBox1.Controls.Clear();
+            }
+            try { N = ToInt32(textBox1.Text); }
+            catch { return; }
+
+
+
+            for (int i = 1; i <= N; i++)
+            {
+                // 15 110 160
+                Label lb = new Label()
+                {
+                    Text = $"請輸入座標點 {i}:",
+                    Location = new Point(15, 30 * i),
+                };
+                TextBox txtX = new TextBox()
+                {
+                    Name = $"X{i}",
+                    Text = "",
+                    Location = new Point(120, 30 * i - 5),
+                    Size = new Size(45, 25)
+                };
+                TextBox txtY = new TextBox()
+                {
+                    Name = $"Y{i}",
+                    Text = "",
+                    Location = new Point(170, 30 * i - 5),
+                    Size = new Size(45, 25)
+                };
+
+                groupBox1.Controls.Add(lb);
+                groupBox1.Controls.Add(txtX);
+                groupBox1.Controls.Add(txtY);
+            }
+        }
+
+        private bool Calc()
+        {
+            (List<double> X, List<double> Y) = getPos();
+            if (X.Count < 1 || Y.Count < 1)
+            {
+                MessageBox.Show("請先輸入資料點", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            double sx, sy;
+            try
+            {
+                sx = ToDouble(textBox2.Text);
+                sy = ToDouble(textBox3.Text);
+            } catch
+            {
+                MessageBox.Show("測試點格式不正確", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            double maxV = double.MinValue;
+            double minV = double.MaxValue;
+            double sum = 0;
+            for (int i = 0; i < X.Count; i++)
+            {
+                maxV = Max(maxV, Sqrt((sx - X[i]) * (sx - X[i]) + (sy - Y[i]) * (sy - Y[i])));
+                minV = Min(minV, Sqrt((sx - X[i]) * (sx - X[i]) + (sy - Y[i]) * (sy - Y[i])));
+                sum += Sqrt((sx - X[i]) * (sx - X[i]) + (sy - Y[i]) * (sy - Y[i]));
+            }
+
+            this.maxV = maxV;
+            this.minV = minV;
+            this.Avr = sum / X.Count;
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Calc())
+                textBox4.Text = $"{maxV:f3}";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Calc())
+                textBox5.Text = $"{minV:f3}";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Calc())
+                textBox6.Text = $"{Avr:f3}";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            (List<double> X, List<double> Y) = getPos();
+            if (X.Count < 1 || Y.Count < 1)
+            {
+                MessageBox.Show("請先輸入資料點", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            double sx, sy;
+            try
+            {
+                sx = ToDouble(textBox2.Text);
+                sy = ToDouble(textBox3.Text);
+            }
+            catch
+            {
+                MessageBox.Show("測試點格式不正確", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            chart1.Visible = true;
+            chart1.Series[1].Points.AddXY(sx, sy);
+            for (int i = 0; i < X.Count; i++)
+            {
+                chart1.Series[0].Points.AddXY(X[i], Y[i]);
+            }
+        }
+    }
+}
