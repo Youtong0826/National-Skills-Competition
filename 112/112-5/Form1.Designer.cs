7b71d8f42ebb7757414b2f042502d44db66af91b
Youtong0826 <jimmy2007826@gmail.com>
2024-11-20 16:41:16 +0800

    add

---
+++ 112/112-5/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace _112_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 70);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 300);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "random blocks";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(256, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "overlapping";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(491, 18);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 35);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 377);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ 112/112-5/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _112_5
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        List<int>[] pts = new List<int>[4];
+        public Form1()
+        {
+            InitializeComponent();
+            g = panel1.CreateGraphics();
+            Console.WriteLine(panel1.Width);
+            Console.WriteLine(panel1.Height);
+        }
+        private bool inside(int x, int y, int s1, int t1, int s2, int t2)
+        {
+            return x < s2 && x > s1 && y < t2 && y > t1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //600 300
+            g.Clear(Color.White);
+            Random rnd = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                int x = rnd.Next(0, 340), y = rnd.Next(0, 130), r = rnd.Next(60, 100);
+
+                pts[i] = new List<int>();
+                pts[i].Add(x);
+                pts[i].Add(y);
+                pts[i].Add(r);
+
+                g.DrawLine(new Pen(Brushes.Black), x, y, x + r, y);
+                g.DrawLine(new Pen(Brushes.Black), x, y, x, y + r);
+                g.DrawLine(new Pen(Brushes.Black), x + r, y + r, x + r, y);
+                g.DrawLine(new Pen(Brushes.Black), x + r, y + r, x, y + r);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= panel1.Width; i++)
+            {
+                for (int j = 0; j <= panel1.Height; j++)
+                {
+                    int cnt = 0;
+                    foreach (var pt in pts)
+                    {
+                        if (inside(i, j, pt[0], pt[1], pt[0] + pt[2], pt[1] + pt[2]))
+                        {
+                            cnt++;
+                        }
+                    }
+
+                    if (cnt >= 2)
+                    {
+                        g.FillRectangle(Brushes.Pink, new Rectangle(i, j, 1, 1));
+                    }
+                }
+            }
+        }
+    }
+}
