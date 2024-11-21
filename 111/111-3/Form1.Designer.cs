eb7a04b834061dc30dc0a4cd8b502d740294bb41
Youtong0826 <jimmy2007826@gmail.com>
2024-11-21 14:09:56 +0800

    111-3

---
+++ 111/111-3/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace _111_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(270, 270);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(299, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(270, 270);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(609, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Random";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(609, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Hor-Flip";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(609, 86);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 31);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Ver-Flip";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(609, 123);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 31);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "CW-Rotate 90";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(609, 160);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(140, 31);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "CCW-Rotate 90";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(609, 197);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(140, 31);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Exit";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 296);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ 111/111-3/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _111_3
+{
+    public partial class Form1 : Form
+    {
+        Graphics g1, g2;
+        Brush[,] mp = new Brush[4, 4]; 
+        public Form1()
+        {
+            InitializeComponent();
+            g1 = panel1.CreateGraphics();
+            g2 = panel2.CreateGraphics();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void randomColored()
+        {
+            Brush[] b =
+            {
+                Brushes.Black, Brushes.Yellow, Brushes.Red, Brushes.Blue,
+                Brushes.Pink, Brushes.Purple, Brushes.Orange, Brushes.SkyBlue,
+                Brushes.Green, Brushes.Gray, Brushes.Brown, Brushes.DarkRed
+            };
+
+            Random rnd = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    mp[i, j] = b[rnd.Next(0, b.Length)];
+                }
+            }
+        }
+
+        private void draw(Graphics g)
+        {
+            int len = 200;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    g.FillRectangle(mp[i, j], new Rectangle(i * len / 4, j * len / 4, len / 4, len / 4));
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // middle
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    var temp = mp[i, j];
+                    mp[i, j] = mp[i, 4 - j - 1];
+                    mp[i, 4 - j - 1] = temp;
+                }
+            }
+            draw(g2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    var temp = mp[i, j];
+                    mp[i, j] = mp[4 - i - 1, j];
+                    mp[4 - i - 1, j] = temp;
+                }
+            }
+            draw(g2);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // 0 0 0 1 0 2 0 3   -> 0 3 1 3 2 3 3 3
+            // 1 0 1 1 1 2 1 3   -> 0 2 1 2
+
+            var temp = new Brush[4, 4];
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    temp[i, j] = mp[i, j];
+                }
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    mp[i, j] = temp[j, 4 - i - 1];
+                }
+            }
+            draw(g2);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var temp = new Brush[4, 4];
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    temp[i, j] = mp[i, j];
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    mp[i, j] = temp[4 - j - 1, i];
+                }
+            }
+
+            draw(g2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            randomColored();
+            draw(g1);
+
+        }
+    }
+}
