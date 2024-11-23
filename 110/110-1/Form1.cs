f65a289902703fe788cf69013917594adb7725d5
Youtong0826 <jimmy2007826@gmail.com>
2024-11-23 23:54:19 +0800

    110-1

---
+++ 110/110-1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _110_1
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        bool[,] mp = new bool[11, 11];
+        List<Tuple<int, int>> list = new List<Tuple<int, int>>();
+        public Form1()
+        {
+            InitializeComponent();
+            g = panel1.CreateGraphics();
+        }
+
+        private void draw()
+        {
+            foreach (var item in list)
+            {
+                int x = item.Item1, y = item.Item2;
+                g.FillEllipse(Brushes.Black, x * 20 + 5, y * 20 + 5, 9, 9);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            list.Clear();
+            for (int i = 0; i < 11; i++) for (int j = 0; j < 11; j++) mp[i, j] = false;
+            for (int i = 0; i < 10; i++)
+            {
+                g.DrawLine(new Pen(Brushes.Black, 2), 10, i * 20 + 10, 190, i * 20 + 10);
+                g.DrawLine(new Pen(Brushes.Black, 2), i * 20 + 10, 10, i * 20 + 10, 190);
+            }
+
+            var rnd = new Random();
+            for (int i = 0; i < 8; i++)
+            {
+                int x = rnd.Next(0, 9), y = rnd.Next(0, 9);
+                mp[x, y] = true;
+                list.Add(new Tuple<int, int>(x, y));
+            }
+            draw();
+        }
+
+        private bool inside(int x, int y)
+        {
+            return x >= 0 && x <= 9 && y >= 0 && y <= 9;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            list.Sort((x, y) => x.Item2.CompareTo(y.Item2));
+            var mid = list[3];
+            int mn = int.MaxValue, mx = int.MinValue;
+            foreach ( var item in list )
+            {
+                g.DrawLine(new Pen(Brushes.Red, 3), item.Item1 * 20 + 10, item.Item2 * 20 + 10, item.Item1 * 20 + 10, mid.Item2 * 20 + 10);
+                mn = Math.Min(mn, item.Item1);
+                mx = Math.Max(mx, item.Item1);
+            }
+
+            g.DrawLine(new Pen(Brushes.Red, 3), mn * 20 + 10, mid.Item2 * 20 + 10, mx * 20 + 10, mid.Item2 * 20 + 10);
+            draw();
+        }
+    }
+}
