33b94cf872eb83d6fd2d5d8961e09c493076ee3e
Youtong0826 <jimmy2007826@gmail.com>
2024-11-25 16:25:40 +0800

    other

---
+++ 其他/番茄鐘/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace 番茄鐘
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 63);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(431, 400);
+            this.panel1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(521, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "分";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(595, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "秒";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(476, 125);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(39, 25);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(549, 125);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(40, 25);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(495, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 36);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "開始計時";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("新細明體", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(476, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 35);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("新細明體", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(186, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 39);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "00:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 513);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 其他/番茄鐘/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+
+namespace 番茄鐘
+{
+    public partial class Form1 : Form
+    {
+        float count = 0;
+        int m, s, total;
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Paint += panel1_Paint;
+            label3.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void draw()
+        {
+            int x = 160, y = 160, r = 150;
+            var g = panel1.CreateGraphics();
+            // g.Clear(Color.White);
+            g.DrawEllipse(new Pen(Brushes.Black, 2), 10, 10, 300, 300);
+            for (float i = 0.5f; i < 61; i++)
+            {
+                g.DrawLine(
+                    new Pen(Brushes.Black),
+                    (r - 10) * (float)Math.Cos(2 * Math.PI * i / 59) + x,
+                    (r - 10) * (float)Math.Sin(2 * Math.PI * i / 59) + y,
+                    r * (float)Math.Cos(2 * Math.PI * i / 59) + x,
+                    r * (float)Math.Sin(2 * Math.PI * i / 59) + y
+                );
+            }
+        }
+
+        private void panel1_Paint(object sender, EventArgs e)
+        {
+            draw();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var g = panel1.CreateGraphics();
+            Console.WriteLine(count);
+            count++;
+            for (float i = (count - 1); i < count; i += 0.01f)
+            {
+                g.DrawLine(new Pen(Brushes.LimeGreen), 160, 160,
+                    160 + 150 * (float)Math.Cos(2 * Math.PI * i / 59),
+                    160 + 150 * (float)Math.Sin(2 * Math.PI * i / 59)
+                );
+            }
+            
+            draw();
+            if (count == 104.5)
+            {
+                timer1.Stop();
+                timer2.Stop();
+                label3.Visible = true;
+                label3.Text = "時間到 !";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var g = panel1.CreateGraphics();
+            g.Clear(Color.White);
+            draw();
+            label3.Visible = false;
+            m = Convert.ToInt32(textBox1.Text); 
+            s = Convert.ToInt32(textBox2.Text);
+            total = m * 60 + s;
+            int tick = total * 1000 / 60;
+            Console.WriteLine(tick);
+            label4.Text = $"{m:d2}:{s:d2}";
+            count = 44.5f;
+            timer1.Interval = tick;
+            timer2.Interval = 1000;
+            timer1.Start();
+            timer2.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            total--;
+            if (total < 0) return;
+            int nm = total / 60, ns = total % 60;
+            label4.Text = $"{nm:d2}:{ns:d2}";
+            
+        }
+    }
+}
